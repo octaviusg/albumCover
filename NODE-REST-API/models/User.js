dfb3bede6c4e90125503363703980c2daad0d5f6
Octavio Garcia <octaviogarciapc@gmail.com>
2021-08-16 23:57:17 -0500

    register get request

--- NODE-REST-API/models/User.js
+++ NODE-REST-API/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
--- NODE-REST-API/routes/auth.js
+++ NODE-REST-API/routes/auth.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("hey its auth route");
+//register
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    consolo.log(err);
+  }
 });
 
 module.exports = router;
